3ec048b697141850213203c05dfeb28f7fe674da
= <7rishi20ss@gmail.com>
2024-06-20 23:44:24 -0600

    Added Prisma client and extension-accelerate dependencies, updated package.json and package-lock.json, created wrangler.toml file, and modified user.ts route to use Prisma client with accelerate extension.

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,9 +1,35 @@
 import {Hono} from 'hono'
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const app = new Hono();
+
+type Variables = {
+    prisma: string
+}
+
+const app = new Hono<{ 
+    Bindings : {
+        DATABASE_URL : string
+    };
+    Variables : Variables 
+ }>();
+
+ app.use('/*',  async (c, next) =>{
+
+    const prisma = new PrismaClient({
+        datasourceUrl : c.env.DATABASE_URL
+    }).$extends(withAccelerate());
+
+    console.log(`Return type of prisma is : ${typeof prisma}`);
+    await next();
+ })
 
 
 app.post('/signup' , (c) => {
+    console.log(c.env.DATABASE_URL)
+    const prisma = new PrismaClient({
+        datasourceUrl : c.env.DATABASE_URL
+    }).$extends(withAccelerate());
     return c.json({
         message: "Welcome to singup page"
     });
