1dea64b13d96d7ac879e6bfe18397eb4bfb6fb2c
= <7rishi20ss@gmail.com>
2024-06-21 17:22:08 -0600

    Added created_at fields to User and Blog models, updated user signup and signin routes to use JWT for authentication, and added JWT_SECRET variable to wrangler.toml

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,6 +1,7 @@
 import {Hono} from 'hono'
-import { PrismaClient } from '@prisma/client/edge';
+import { PrismaClient, } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
+import { sign } from 'hono/jwt';
 
 
 type Variables = {
@@ -16,6 +17,7 @@ type User = {
 const app = new Hono<{ 
     Bindings : {
         DATABASE_URL : string
+        JWT_SECRET : string
     };
     Variables : Variables 
  }>();
@@ -38,34 +40,85 @@ app.post('/signup' ,async  (c) => {
     }).$extends(withAccelerate());
    
     try {
-        const user : User = await c.req.json();
+        const body : User = await c.req.json();
         const response = await prisma.user.create({
             data : {
-                username : user.username,
-                password : user.password,
-                name : user.name
+                username : body.username,
+                password : body.password,
+                name : body.name
             }, 
         });
 
-        console.log(`Response after singup is : ${JSON.stringify(response)}`);
+        // console.log(`Response after singup is : ${JSON.stringify(response)}`);
+
+        // use jwt to sign uderid
+        const payload = {
+            userId : response.id,
+        }
+        const token : string = await sign(payload, c.env.JWT_SECRET);
+        // console.log(`Token generated is : ${token}`);
 
         return c.json({
             message : 'User created successfully',
-            data : response
+            data : response,
+            token : token
         }, 200);
 
     } catch (error) {
         return c.json({
-            message : error
-        }, 400);
+            message : `error while signup!! User Already Exists`,
+            error : error
+        }, 409);
     }
 
 });
 
-app.post('/signin' , (c) => {
-    return c.json({
-        message: "Welcome to signin page"
-    });
-})
+app.post('/signin' , async (c) => {
+    console.log(c.env.DATABASE_URL)
+    const prisma = new PrismaClient({
+        datasourceUrl : c.env.DATABASE_URL
+    }).$extends(withAccelerate());
+
+    try {
+        const body : User = await c.req.json();
+        const user = await prisma.user.findFirst({
+            where : {
+                username : body.username,
+                password : body.password
+            }, select : {
+                id : true,
+                username : true,
+                name : true
+            }
+        });
+
+        if( !user ) {
+            c.status(403);
+            return c.json({
+                message : 'No user found?'
+            });
+        }
+
+        // sign with jwt
+        const payload = {
+            userId : user.id,
+        }
+        const token: string = await sign(payload, c.env.JWT_SECRET);
+        console.log(`Token generated is : ${token}`);
+        
+        return c.json({
+            message : 'User signed in successfully',
+            data : user,
+            token : token
+        });
+
+    } catch (err) {
+        c.status(401);
+        return c.json({
+            message : 'Invalid Credentials',
+            error : err
+        });
+    }
+});
 
 export default app;
