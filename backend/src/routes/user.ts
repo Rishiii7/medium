2db1b147e1d142adea5ebdf7abb7a74a4cca7e37
= <7rishi20ss@gmail.com>
2024-06-21 16:39:33 -0600

    "Added User type definition, modified middleware to remove PrismaClient initialization, and updated /signup endpoint to create user with PrismaClient"

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -7,6 +7,12 @@ type Variables = {
     prisma: string
 }
 
+type User = {
+    username : string;
+    password : string;
+    name? :  string;
+}
+
 const app = new Hono<{ 
     Bindings : {
         DATABASE_URL : string
@@ -14,25 +20,45 @@ const app = new Hono<{
     Variables : Variables 
  }>();
 
- app.use('/*',  async (c, next) =>{
+//  app.use('/*',  async (c, next) =>{
 
-    const prisma = new PrismaClient({
-        datasourceUrl : c.env.DATABASE_URL
-    }).$extends(withAccelerate());
+//     const prisma : PrismaClient = new PrismaClient({
+//         datasourceUrl : c.env.DATABASE_URL
+//     }).$extends(withAccelerate());
 
-    console.log(`Return type of prisma is : ${typeof prisma}`);
-    await next();
- })
+//     console.log(`Return type of prisma is : ${typeof prisma}`);
+//     await next();
+//  })
 
 
-app.post('/signup' , (c) => {
+app.post('/signup' ,async  (c) => {
     console.log(c.env.DATABASE_URL)
     const prisma = new PrismaClient({
         datasourceUrl : c.env.DATABASE_URL
     }).$extends(withAccelerate());
-    return c.json({
-        message: "Welcome to singup page"
-    });
+   
+    try {
+        const user : User = await c.req.json();
+        const response = await prisma.user.create({
+            data : {
+                username : user.username,
+                password : user.password,
+                name : user.name
+            }, 
+        });
+
+        console.log(`Response after singup is : ${JSON.stringify(response)}`);
+
+        return c.json({
+            message : 'User created successfully',
+            data : response
+        }, 200);
+
+    } catch (error) {
+        return c.json({
+            message : error
+        }, 400);
+    }
 
 });
 
