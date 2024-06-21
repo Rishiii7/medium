f96e925cba1eed97bcd0a2823c3c6f0ed6580b68
= <7rishi20ss@gmail.com>
2024-06-20 21:27:03 -0600

    Added POST, PUT, GET /:id, and GET /bulk routes to blog.ts

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -3,6 +3,29 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+app.post('/' , (c) => {
+    return c.json({
+        message : "FROM blog POST"
+    });
+});
 
+app.put('/', (c) => {
+    return c.json({
+        message: "FROM blog PUT"
+    });
+});
 
-export default app
+app.get('/:id', (c) => {
+    const id = c.req.param('id');
+    return c.json({
+        message: `FROM blog GET ${id}`  
+    });
+});
+
+app.get('/bulk', (c) => {
+    return c.json({
+        message : `got all the blogs`
+    });
+});
+
+export default app;
