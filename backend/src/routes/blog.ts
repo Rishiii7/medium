f41b1194bb89efe2d2d41b1e6936e5a81b8d0cdd
= <7rishi20ss@gmail.com>
2024-06-21 19:06:50 -0600

    Added Prisma client and JWT secret to blog route, updated Wrangler config with JWT secret

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,12 +1,58 @@
 // blog.ts
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 import { Hono } from 'hono'
 
-const app = new Hono()
+type Bindings = {
+    DATABASE_URL : string
+    JWT_SECRET : string
+}
 
-app.post('/' , (c) => {
-    return c.json({
-        message : "FROM blog POST"
-    });
+type Blog = {
+    title : string;
+    content : string;
+    authorId : string;
+}
+
+type Variables = {
+    // Add your variables here
+}
+
+const app = new Hono<{
+    Bindings: Bindings,
+    Variables: Variables
+}>();
+
+app.post('/' , async (c) => {
+
+    const prisma =  new PrismaClient({
+        datasourceUrl : c.env.DATABASE_URL
+    }).$extends(withAccelerate());
+
+    try {
+        const body : Blog = await c.req.json();
+
+        const response = await prisma.blog.create({
+            data : {
+                title : body.title,
+                content : body.content,
+                authorId : body.authorId
+            },
+        });
+
+        console.log(`Reponse is : ${response}`);
+
+        return c.json({
+            message : 'Blog created successfully',
+            data : response
+        });
+    } catch (err) {
+        console.error(err);
+        return c.json({
+            message : 'Error creating blog',
+            error : err
+        });
+    }
 });
 
 app.put('/', (c) => {
